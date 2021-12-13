582275d826b8a1ddf0df1bcec3f57c90c034abff
kinori9 <kinori9@gmail.com>
2021-12-14 05:20:47 +0900

    num_plus

---
+++ level1/num_plus/main.py
@@ -0,0 +1,13 @@
+def solution(numbers):
+    answer = 0
+    arr = [-1] * 10
+    for idx, val in enumerate(numbers):
+        arr[val] = 1
+    print(arr)
+    for idx, val in enumerate(arr):
+        if arr[idx] == -1:
+            answer += idx
+    return answer
+
+arr = [5, 8, 4, 0, 6, 7, 9]
+print(solution(arr))
